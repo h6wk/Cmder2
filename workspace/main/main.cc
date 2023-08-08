1c136ac67dac3a7719ffac080534e456aa284476
h6wk <h6wking@gmail.com>
2023-08-08 08:41:47 +0000

    Remove greet.h and add package status snapshot

--- workspace/main/main.cc
+++ workspace/main/main.cc
@@ -1,13 +1,12 @@
 /******************************************************************************
  * @Author                : h6wk<h6wking@gmail.com>                           *
  * @CreatedDate           : 2023-07-31 20:37:40                               *
- * @LastEditDate          : 2023-07-31 20:37:48                               *
+ * @LastEditDate          : 2023-08-08 08:30:39                               *
  * @CopyRight             : GNU GPL                                           *
  *****************************************************************************/
 
-#include "workspace/tools/greet.h"
 
 int main()
 {
-    greeter::greet();
+    return 0;
 }
