9e455942ced3f3b12c2fbce9fe50b0381111bc37
h6wk <h6wking@gmail.com>
2024-02-11 21:24:05 +0000

    MpExecutor class to work on command execution

---
+++ workspace/exe/MpExecutor.cc
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * @Author                : h6wk<h6wking@gmail.com>                           *
+ * @CreatedDate           : 2024-02-11 21:18:42                               *
+ * @LastEditDate          : 2024-02-11 21:21:06                               *
+ * @CopyRight             : GNU GPL                                           *
+ *****************************************************************************/
+
+#include <exe/MpExecutor.h>
+
+namespace cmder::exe {
+
+  MpExecutor::MpExecutor()
+  {
+  }
+
+  MpExecutor::~MpExecutor()
+  {
+  }
+}
---
+++ workspace/exe/MpExecutor.h
@@ -0,0 +1,20 @@
+/******************************************************************************
+ * @Author                : h6wk<h6wking@gmail.com>                           *
+ * @CreatedDate           : 2024-02-11 21:16:11                               *
+ * @LastEditDate          : 2024-02-11 21:18:15                               *
+ * @CopyRight             : GNU GPL                                           *
+ *****************************************************************************/
+
+#ifndef FBDBCD46_4823_4132_8C5A_00FA743A0731
+#define FBDBCD46_4823_4132_8C5A_00FA743A0731
+
+namespace cmder::exe {
+
+  class MpExecutor {
+  public:
+    MpExecutor();
+    virtual ~MpExecutor();
+  };
+}
+
+#endif /* FBDBCD46_4823_4132_8C5A_00FA743A0731 */
